f6e7b9adf2f7a2bdbbe538ab21e94a0d41e79787
Jhenyu <jhenyu.lin@getac.com.tw>
2025-10-03 15:43:36 +0800

    fix hello world import

--- airflow/dags/hello_world_dag.py
+++ airflow/dags/hello_world_dag.py
@@ -1,13 +1,13 @@
 # Test DAG by printing Hello World every 2 minutes
 from datetime import datetime
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.providers.cncf.kubernetes.secret import Secret
 
 with DAG(
     dag_id="hello_world",
     start_date=datetime(2025, 10, 2),
-    schedule="@daily",
+    schedule="*/2 * * * *" ,
     catchup=False,
 ) as dag:
 
@@ -19,7 +19,6 @@ with DAG(
         cmds=["echo", "Hello World"],
         get_logs=True,
         is_delete_operator_pod=True,
-        in_cluster=True,
         secrets=[
             Secret("env", "S3_ENDPOINT_URL", "textpipe-secrets", "S3_ENDPOINT_URL"),
             Secret("env", "AWS_ACCESS_KEY_ID", "textpipe-secrets", "AWS_ACCESS_KEY_ID"),
