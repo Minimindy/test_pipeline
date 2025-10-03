c3f885c85ebc441c9a560c4414b5f5d5923d3ebe
Jhenyu <jhenyu.lin@getac.com.tw>
2025-10-03 15:52:49 +0800

    delete secret part

--- airflow/dags/hello_world_dag.py
+++ airflow/dags/hello_world_dag.py
@@ -18,10 +18,5 @@ with DAG(
         image="alpine:latest",
         cmds=["echo", "Hello World"],
         get_logs=True,
-        is_delete_operator_pod=True,
-        secrets=[
-            Secret("env", "S3_ENDPOINT_URL", "textpipe-secrets", "S3_ENDPOINT_URL"),
-            Secret("env", "AWS_ACCESS_KEY_ID", "textpipe-secrets", "AWS_ACCESS_KEY_ID"),
-            Secret("env", "AWS_SECRET_ACCESS_KEY", "textpipe-secrets", "AWS_SECRET_ACCESS_KEY"),
-        ],
+        is_delete_operator_pod=True
     )
